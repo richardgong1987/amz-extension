fc03780955cf744708f8bbf034bde65816d6d14c
pc <pc@pc.com>
2023-09-28 00:28:36 +0900

    clear store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
+import {Utils} from "src/utils/utils";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'auctions-yahoo-extension';
+  title = "auctions-yahoo-extension";
+
+  async storeClear() {
+    var isclear = confirm("确定要清除缓存吗?")
+    if (isclear) {
+      Utils.storeClear();
+    }
+  }
 }
