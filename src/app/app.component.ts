912f2895721e7dde33c8bcee8cbcc1efbc36167c
pc <pc@pc.com>
2023-09-28 08:14:48 +0900

    clear store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {Utils} from "src/utils/utils";
 
 @Component({
@@ -6,13 +6,21 @@ import {Utils} from "src/utils/utils";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "auctions-yahoo-extension";
 
   async storeClear() {
     var isclear = confirm("确定要清除缓存吗?")
     if (isclear) {
       Utils.storeClear();
+      this.ngOnInit();
     }
+  }
+
+  storeString = "";
+
+  async ngOnInit() {
+    let newVar = await Utils.storeGetAll();
+    this.storeString = JSON.stringify(newVar);
   }
 }
