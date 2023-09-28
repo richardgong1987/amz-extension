27526eb60ce7d50e766222f4b35884346591f778
pc <pc@pc.com>
2023-09-28 12:50:20 +0900

    clear store

--- p.sh
+++ p.sh
@@ -7,3 +7,14 @@ rm auctions-yahoo-extension.zip
 zip -r auctions-yahoo-extension.zip auctions-yahoo-extension/
 
 mv auctions-yahoo-extension.zip ../../auctions-yahoo-admin-ui/public
+
+
+cd ../../auctions-yahoo-admin-ui
+
+pnpm run build:prod
+
+rm ruoyi-admin-ui.zip
+
+zip -r dist.zip dist/
+
+open .
