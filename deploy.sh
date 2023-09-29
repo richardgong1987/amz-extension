40f021fc001442a51469eb5682de758d4c70f6e1
pc <pc@pc.com>
2023-09-29 22:03:30 +0900

    updated

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+sh p.sh
+
+scp -P 32469 -i /Users/richard/.ssh/rakuden_apple.pem /Users/richard/IdeaProjects/auctions-yahoo-group/auctions-yahoo-admin-ui/public/auctions-yahoo-extension.zip rdalma@ik1-215-78366.vs.sakura.ne.jp:/www/wwwroot/default/dist/
--- p.sh
+++ p.sh
@@ -7,14 +7,3 @@ rm auctions-yahoo-extension.zip
 zip -r auctions-yahoo-extension.zip auctions-yahoo-extension/
 
 mv auctions-yahoo-extension.zip ../../auctions-yahoo-admin-ui/public
-
-
-cd ../../auctions-yahoo-admin-ui
-
-pnpm run build:prod
-
-rm ruoyi-admin-ui.zip
-
-zip -r dist.zip dist/
-
-open .
