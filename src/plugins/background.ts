12121be67b1b8f469781cc0e20cf3641ea796aad
pc <pc@pc.com>
2023-10-03 20:00:22 +0900

    save the timeleft

--- src/plugins/background.ts
+++ src/plugins/background.ts
@@ -2,14 +2,14 @@ import {Utils} from "src/utils/utils";
 
 const setTimeoutMap = new Map<string, any>()
 
-function getAllTabs() {
-    chrome.tabs.query({}, (tabs) => {
-        refreshTab(tabs)
-        activateTheUpComingTab(tabs);
-    })
+
+function getAllTabs(complete = function (tabs: chrome.tabs.Tab[]) {
+}) {
+    chrome.tabs.query({}, complete)
 }
 
 async function activateTheUpComingTab(tabs: chrome.tabs.Tab[]) {
+    console.log("***Tabs:", tabs);
     let id = await getUpcommingInfo();
     if (id) {
         activateTab(searchTabByUrl(id, tabs) as chrome.tabs.Tab)
@@ -19,7 +19,7 @@ async function activateTheUpComingTab(tabs: chrome.tabs.Tab[]) {
 function searchTabByUrl(id: string, tabs: chrome.tabs.Tab[]) {
     for (const tab of tabs) {
         if (tab.id && isinAuction(tab)) {
-            let locate = new URL(tab.url as string);
+            let locate = getURL(tab);
             if (locate.pathname.split("/").pop() == id) {
                 return tab
             }
@@ -73,29 +73,49 @@ function mysetTimeOut(tab: chrome.tabs.Tab) {
 
 }
 
-let refreshInterval = setInterval(refreshInactiveTabs, 2 * 60 * 1000);
 
 function refreshInactiveTabs() {
-    getAllTabs();
+    getAllTabs(refreshTab);
+}
+
+function activeUpComingTabs() {
+    getAllTabs(activateTheUpComingTab);
 }
 
 
 function isinAuction(tab: chrome.tabs.Tab) {
     if (tab.id) {
-        let locate = new URL(tab.url as string);
+        getURL(tab);
+        let locate = getURL(tab);
         return /^\/jp\/auction\/[a-z][0-9]{10}$/.test(locate.pathname)
     }
     return false;
 }
 
+function getURL(tab: chrome.tabs.Tab) {
+    try {
+        return new URL(tab.url as string);
+    } catch (e) {
+    }
+    return {
+        pathname: ""
+    }
+
+}
+
+const REFRESH_TIME = 2 * 60 * 1000;
+const UPCOMMING_TIME = 3 * 1000;
+let refreshInterval = setInterval(refreshInactiveTabs, REFRESH_TIME);
+let upCommingInterval = setInterval(activeUpComingTabs, UPCOMMING_TIME);
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log("****message:", message)
     if (message.action === "startRefresh") {
-        // Start refreshing tabs every 5 seconds
         clearInterval(refreshInterval);
-        refreshInterval = setInterval(refreshInactiveTabs, 2 * 60 * 1000);
-    } else if (message.action === "stopRefresh") {
-        // Stop the refreshing
+        clearInterval(upCommingInterval);
+        refreshInterval = setInterval(refreshInactiveTabs, REFRESH_TIME);
+        upCommingInterval = setInterval(activeUpComingTabs, UPCOMMING_TIME);
+    } else {
         clearInterval(refreshInterval);
+        clearInterval(upCommingInterval);
     }
 });
