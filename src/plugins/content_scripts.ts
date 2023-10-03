92e08a716bffe2a4c9f95ecd87c3a56e279280ec
pc <pc@pc.com>
2023-10-03 12:09:00 +0900

    save the timeleft

--- src/plugins/content_scripts.ts
+++ src/plugins/content_scripts.ts
@@ -2,90 +2,90 @@ import {Biz} from "src/utils/biz";
 import {Utils} from "src/utils/utils";
 
 export class JqGet {
-  async init() {
-    this.pInfo = Biz.getProductInformation() as { [p: string]: string }
-    console.log("****pInfo:", await Utils.storeGetAll());
-    await this.main();
-  }
-
-  pInfo: { [x: string]: string; } = {}
-  orderDetail: { [x: string]: any; } = {}
-
-  async main() {
-    const productInformation = this.pInfo;
-    if (!productInformation) {
-      Biz.otherPage();
-      return console.log("****productInformation is not exit:", productInformation);
+    async init() {
+        this.pInfo = Biz.getProductInformation() as { [p: string]: string }
+        console.log("****pInfo:", await Utils.storeGetAll());
+        await this.main();
     }
 
-    const orderDetail = this.orderDetail = await Biz.orderDetail(productInformation["オークションID"]);
-    if (!orderDetail || orderDetail.status != 1) {
-      if (!orderDetail) {
-        Biz.showAddJobButton(productInformation);
-      }
-      clearJob()
-      return console.log("****orderDetail is failure:", orderDetail);
-    }
-    this.pInfo["limitPrice"] = orderDetail.limitPrice
-    this.pInfo["url"] = orderDetail.url
-    this.pInfo["status"] = orderDetail.status
-    if (Biz.ifSuccess(orderDetail)) {
-      clearJob()
-      return console.log("****this order already success:", orderDetail);
-    }
-    let old = await Utils.storeGet(orderDetail.orderId) || {};
-    await Utils.storePut(orderDetail.orderId, Object.assign(old, this.pInfo));
-    if (Number($(".Price__value").text().split("円").shift()?.replace(/,/g, "")) >= orderDetail.limitPrice) {
-      Biz.overPrice(this.orderDetail["orderId"])
-      clearJob()
-      return alert("main已超出最高价,30秒后关页面")
-    }
-    return Biz.updateProdctAjax({
-      orderId: orderDetail.orderId,
-      updateTime: Utils.formatDateStr(productInformation["終了日時"])
-    })
-  }
+    pInfo: { [x: string]: string; } = {}
+    orderDetail: { [x: string]: any; } = {}
 
-  offerBid(day: number, hour: number, min: number, sec: number) {
-    //  check if the time is correct
-    const othersBidCount = Number($(".Count .Count__link .Count__detail").text().slice(0, -1))
-    if (othersBidCount > 1) {
-      if (!(day == 0 && hour == 0 && min == 0 && sec <= 5)) {
-        return;
-      }
-    } else {
-      if (!(day == 0 && hour == 0 && min == 0 && sec <= 3)) {
-        return;
-      }
-    }
-    // check order status
-    // @ts-ignore
-    if (!this.orderDetail || this.orderDetail.status != 1) {
-      return;
-    }
-    // check currently prices
-    // @ts-ignore
-    if (Number($(".Price__value").text().split("円").shift()?.replace(/,/g, "")) >= this.orderDetail.limitPrice) {
-      Biz.overPrice(this.orderDetail["orderId"])
-      clearJob()
-      return alert("offerBid已超出最高价,30秒后关页面")
+    async main() {
+        const productInformation = this.pInfo;
+        if (!productInformation) {
+            Biz.otherPage();
+            return console.log("****productInformation is not exit:", productInformation);
+        }
+
+        const orderDetail = this.orderDetail = await Biz.orderDetail(productInformation["オークションID"]);
+        if (!orderDetail || orderDetail.status != 1) {
+            if (!orderDetail) {
+                Biz.showAddJobButton(productInformation);
+            }
+            clearJob()
+            return console.log("****orderDetail is failure:", orderDetail);
+        }
+        this.pInfo["limitPrice"] = orderDetail.limitPrice
+        this.pInfo["url"] = orderDetail.url
+        this.pInfo["status"] = orderDetail.status
+        if (Biz.ifSuccess(orderDetail)) {
+            clearJob()
+            return console.log("****this order already success:", orderDetail);
+        }
+        let old = await Utils.storeGet(orderDetail.orderId) || {};
+        await Utils.storePut(orderDetail.orderId, Object.assign(old, this.pInfo));
+        if (Number($(".Price__value").text().split("円").shift()?.replace(/,/g, "")) >= orderDetail.limitPrice) {
+            Biz.overPrice(this.orderDetail["orderId"])
+            clearJob()
+            return alert("main已超出最高价,30秒后关页面")
+        }
+        return Biz.updateProdctAjax({
+            orderId: orderDetail.orderId,
+            updateTime: Utils.formatDateStr(productInformation["終了日時"])
+        })
     }
 
+    offerBid(day: number, hour: number, min: number, sec: number) {
+        //  check if the time is correct
+        const othersBidCount = Number($(".Count .Count__link .Count__detail").text().slice(0, -1))
+        if (othersBidCount > 1) {
+            if (!(day == 0 && hour == 0 && min == 0 && sec <= 5)) {
+                return;
+            }
+        } else {
+            if (!(day == 0 && hour == 0 && min == 0 && sec <= 3)) {
+                return;
+            }
+        }
+        // check order status
+        // @ts-ignore
+        if (!this.orderDetail || this.orderDetail.status != 1) {
+            return;
+        }
+        // check currently prices
+        // @ts-ignore
+        if (Number($(".Price__value").text().split("円").shift()?.replace(/,/g, "")) >= this.orderDetail.limitPrice) {
+            Biz.overPrice(this.orderDetail["orderId"])
+            clearJob()
+            return alert("offerBid已超出最高价,30秒后关页面")
+        }
 
-    console.log("******************************************************************************************开始抢了:");
-    //1. bid
-    Biz.bid();
-    //2. can not upper the limit price
-    if (!Biz.isGoodPrice(this.orderDetail["limitPrice"])) {
-      Biz.overPrice(this.orderDetail["orderId"])
-      clearJob()
-      return alert("offerBid已超出最高价,30秒后关闭页面");
+
+        console.log("******************************************************************************************开始抢了:");
+        //1. bid
+        Biz.bid();
+        //2. can not upper the limit price
+        if (!Biz.isGoodPrice(this.orderDetail["limitPrice"])) {
+            Biz.overPrice(this.orderDetail["orderId"])
+            clearJob()
+            return alert("offerBid已超出最高价,30秒后关闭页面");
+        }
+        setTimeout(function () {
+            Utils.clickWithSelector(".js-validator-submit");
+        }, 1);
+        //3. 確認する
     }
-    setTimeout(function () {
-      Utils.clickWithSelector(".js-validator-submit");
-    }, 1);
-    //3. 確認する
-  }
 }
 
 let auctionId = location.pathname.split("/").pop() as string;
@@ -96,7 +96,7 @@ let outputString = "";
 var setTmp = setInterval(timePaint, 1000);
 
 function clearJob() {
-  clearInterval(setTmp);
+    clearInterval(setTmp);
 }
 
 const xmlhttp = createXMLHttp();
@@ -106,100 +106,105 @@ myInstance.init();
 timePaint();
 
 function createXMLHttp() {
-  try {
-    // @ts-ignore
-    return new ActiveXObject("Msxml2.XMLHTTP");
-  } catch (e) {
     try {
-      // @ts-ignore
-      return new ActiveXObject("Microsoft.XMLHTTP");
+        // @ts-ignore
+        return new ActiveXObject("Msxml2.XMLHTTP");
     } catch (e) {
-      try {
-        return new XMLHttpRequest();
-      } catch (e) {
-      }
+        try {
+            // @ts-ignore
+            return new ActiveXObject("Microsoft.XMLHTTP");
+        } catch (e) {
+            try {
+                return new XMLHttpRequest();
+            } catch (e) {
+            }
+        }
     }
-  }
-  return null;
+    return null;
 }
 
 function setPageData(xmlhttp: XMLHttpRequest) {
-  try {
-    //readyState complete(4) is OK
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      // @ts-ignore
-      timeLeft = xmlhttp.responseText;
-      Biz.saveAuctionLefttime(auctionId, +timeLeft)
-    }
-  } catch (e) {
+    try {
+        //readyState complete(4) is OK
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            // @ts-ignore
+            timeLeft = xmlhttp.responseText;
+            Biz.saveAuctionLefttime(auctionId, +timeLeft)
+        }
+    } catch (e) {
 
-  }
+    }
 }
 
 function checkObject() {
-  if (xmlhttp) {
-    try {
-      xmlhttp.abort();
-      var nowTime = new Date().getTime();
-      xmlhttp.onreadystatechange = function () {
-        setPageData(xmlhttp)
-      };
-      xmlhttp.open("GET", "https://page.auctions.yahoo.co.jp/now?aID=" + auctionId + "&nowtime=" + nowTime, true);
-      xmlhttp.send(null);
+    if (xmlhttp) {
+        try {
+            xmlhttp.abort();
+            var nowTime = new Date().getTime();
+            xmlhttp.onreadystatechange = function () {
+                setPageData(xmlhttp)
+            };
+            xmlhttp.open("GET", "https://page.auctions.yahoo.co.jp/now?aID=" + auctionId + "&nowtime=" + nowTime, true);
+            xmlhttp.send(null);
 
-    } catch (e) {
-      if (!isFirstPaint) {
-        window.location.reload();
-      }
-    }
+        } catch (e) {
+            if (!isFirstPaint) {
+                window.location.reload();
+            }
+        }
 
-  } else {
-    if (!isFirstPaint) {
-      window.location.reload();
+    } else {
+        if (!isFirstPaint) {
+            window.location.reload();
+        }
     }
-  }
 }
 
 function timePaint() {
-  if (!/^\/jp\/auction\/[a-z][0-9]{10}$/.test(location.pathname) || $(".ClosedHeader__tag").text() == "このオークションは終了しています") {
-    return clearInterval(setTmp);
-  }
-
-  if (isFirstPaint || timeLeft == -1 || (timeLeft < 300 && timeSinceLast >= 60)) {
-    if ((timeLeft < 75 && timeSinceLast >= 60)) {
-      return window.location.reload();
+    if (!/^\/jp\/auction\/[a-z][0-9]{10}$/.test(location.pathname) || $(".ClosedHeader__tag").text() == "このオークションは終了しています") {
+        return clearInterval(setTmp);
     }
-    checkObject();
-    isFirstPaint = false;
-    timeSinceLast = 0;
-  }
 
-  if (timeLeft === -10) {
-    return;
-  }
+    if (timeLeft > 300 && timeSinceLast >= 60 * 5) {
+        return window.location.reload();
+    }
+    
+    if (isFirstPaint || timeLeft == -1 || (timeLeft < 300 && timeSinceLast >= 60)) {
 
-  if (timeLeft <= 0) {
-    outputString = "オークション - 終了";
-  } else {
-    var day = Math.floor(timeLeft / 86400);
-    var hour = Math.floor((timeLeft - day * 86400) / 3600);
-    var min = Math.floor((timeLeft - (day * 86400) - (hour * 3600)) / 60);
-    var sec = timeLeft - (day * 86400) - (hour * 3600) - (min * 60);
+        if ((timeLeft < 75 && timeSinceLast >= 60)) {
+            return window.location.reload();
+        }
+        checkObject();
+        isFirstPaint = false;
+        timeSinceLast = 0;
+    }
 
+    if (timeLeft === -10) {
+        return;
+    }
 
-    if (day > 0) {
-      outputString = day + "日＋" + ((hour > 0) ? hour + ":" : "") + ((min < 10) ? "0" + min : min) + ":" + ((sec < 10) ? "0" + sec : sec);
+    if (timeLeft <= 0) {
+        outputString = "オークション - 終了";
     } else {
-      outputString = ((hour > 0) ? hour + ":" : "") + ((min < 10) ? "0" + min : min) + ":" + ((sec < 10) ? "0" + sec : sec);
-    }
-    timeLeft -= 1;
-    timeSinceLast += 1;
+        var day = Math.floor(timeLeft / 86400);
+        var hour = Math.floor((timeLeft - day * 86400) / 3600);
+        var min = Math.floor((timeLeft - (day * 86400) - (hour * 3600)) / 60);
+        var sec = timeLeft - (day * 86400) - (hour * 3600) - (min * 60);
 
-    if (outputString != "オークション - 終了") {
-      myInstance.offerBid(day, hour, min, sec);
+
+        if (day > 0) {
+            outputString = day + "日＋" + ((hour > 0) ? hour + ":" : "") + ((min < 10) ? "0" + min : min) + ":" + ((sec < 10) ? "0" + sec : sec);
+        } else {
+            outputString = ((hour > 0) ? hour + ":" : "") + ((min < 10) ? "0" + min : min) + ":" + ((sec < 10) ? "0" + sec : sec);
+        }
+        timeLeft -= 1;
+        timeSinceLast += 1;
+
+        if (outputString != "オークション - 終了") {
+            myInstance.offerBid(day, hour, min, sec);
+        }
     }
-  }
-  console.log("****残り時間:", outputString)
+    console.log("****残り時間:", outputString)
 }
 
 
