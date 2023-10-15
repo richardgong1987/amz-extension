67f47d78b14ad7a8ffb7c7eb67bc1df4ad995ae8
pc <pc@pc.com>
2023-10-15 20:10:58 +0900

    new UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import {Component, OnInit} from "@angular/core";
-import {IBidList, StatusDict} from "src/app/data/interface";
+import {LiveAnnouncer} from "@angular/cdk/a11y";
+import {AfterViewInit, Component, OnInit, ViewChild} from "@angular/core";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatTableDataSource} from "@angular/material/table";
+import {IBidItem, IBidList, StatusDict} from "src/app/data/interface";
+import {Utils} from "src/utils/utils";
 
 
 @Component({
@@ -7,9 +11,17 @@ import {IBidList, StatusDict} from "src/app/data/interface";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = ["orderId", "info", "timeLeft", "limitPrice", "status", "remark", "operation"];
   dataSource = IBidList;
+
+  ngAfterViewInit() {
+    // @ts-ignore
+    this.dataSource.paginator = this.paginator;
+  }
+
+  constructor() {
+  }
 
   async storeClear() {
     var isclear = confirm("确定要清除缓存吗?")
@@ -26,14 +38,6 @@ export class AppComponent implements OnInit {
   async ngOnInit() {
   }
 
-  startRefresh() {
-    this.port.postMessage({action: "startRefresh"});
-  }
-
-  stopRefresh() {
-    this.port.postMessage({action: "stopRefresh"});
-  }
-
   translateStatus(value: string) {
     for (let {dictValue, dictLabel, listClass} of StatusDict) {
       if (dictValue == value) {
@@ -41,5 +45,15 @@ export class AppComponent implements OnInit {
       }
     }
     return {dictValue: "", dictLabel: "", listClass: ""}
+  }
+
+  delete(item: IBidItem) {
+    if (confirm("本当に削除しますか?")) {
+      Utils.STORE_DELETE_ITEM(item.orderId);
+    }
+  }
+
+  edit(element: IBidItem) {
+    console.log(element);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from "@angular/core";
 import {MatButtonModule} from "@angular/material/button";
+import {MatPaginatorModule} from "@angular/material/paginator";
 import {MatTableModule} from "@angular/material/table";
 import {MatTabsModule} from "@angular/material/tabs";
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from "@angular/platform-browser";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
-import { AppComponent } from './app.component';
+import {AppComponent} from "./app.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatTableModule,
     MatTabsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
