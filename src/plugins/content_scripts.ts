b2afac45547cc1d9bbe2c4811312ae2e297758a8
pc <pc@pc.com>
2023-10-20 23:28:58 +0900

    fixed bug

--- src/plugins/content_scripts.ts
+++ src/plugins/content_scripts.ts
@@ -3,110 +3,111 @@ import {Utils} from "src/utils/utils";
 
 
 export class JqGet {
-  pInfo: { [x: string]: string | number; } = {}
-  orderDetail: { [x: string]: any; } = {}
+    pInfo: { [x: string]: string | number; } = {}
+    orderDetail: { [x: string]: any; } = {}
 
-  async main() {
-    const productInformation = this.pInfo = Biz.getProductInformation() as { [p: string]: string }
-    // console.log("****all  store:", await Utils.STORE_GET_ALL());
-    if (!productInformation) {
-      Biz.otherPage();
-      return console.log("****productInformation is not exit:", productInformation);
-    }
-    let infoId = productInformation["オークションID"];
-    const orderDetail = this.orderDetail = await Utils.STORE_GET_ITEM(infoId);
-    if (orderDetail.status !== undefined) {
-      this.createConnect();
-    }
-    if (!orderDetail || orderDetail.status != 1) {
-      if (orderDetail.status === undefined) {
-        Biz.showAddJobButton(productInformation);
-      } else {
-        if (orderDetail.status == 5) {
-          this.pInfo["status"] = orderDetail.status
-          Biz.ifSuccess(Object.assign(this.pInfo, orderDetail));
+    async main() {
+        const productInformation = this.pInfo = Biz.getProductInformation() as { [p: string]: string }
+        // console.log("****all  store:", await Utils.STORE_GET_ALL());
+        if (!productInformation) {
+            Biz.otherPage();
+            return console.log("****productInformation is not exit:", productInformation);
         }
-        Biz.disconnect();
-      }
-      return console.log("****orderDetail is failure:", orderDetail);
+        let infoId = productInformation["オークションID"];
+        const orderDetail = this.orderDetail = await Utils.STORE_GET_ITEM(infoId);
+        if (orderDetail.status !== undefined) {
+            this.createConnect();
+        }
+        if (!orderDetail || orderDetail.status != 1) {
+            if (orderDetail.status === undefined) {
+                Biz.showAddJobButton(productInformation);
+            } else {
+                if (orderDetail.status == 5) {
+                    this.pInfo["status"] = orderDetail.status
+                    Biz.ifSuccess(Object.assign(this.pInfo, orderDetail));
+                }
+                Biz.disconnect();
+            }
+            return console.log("****orderDetail is failure:", orderDetail);
+        }
+
+        this.pInfo["limitPrice"] = orderDetail.limitPrice
+        this.pInfo["status"] = orderDetail.status
+        this.pInfo["orderId"] = orderDetail.orderId
+        if (Biz.ifSuccess(this.pInfo)) {
+            return console.log("****this order already success:", orderDetail);
+        }
+        let old = await Utils.STORE_GET_ITEM(orderDetail.orderId);
+        old.updateTime = Utils.formatDateStr(productInformation["終了日時"]);
+        await Utils.STORE_SET_ITEM(orderDetail.orderId, Object.assign(old, this.pInfo));
+        const pageData = Utils.getPageDataJSON();
+        if (Number(pageData?.items?.price) > orderDetail.limitPrice) {
+            Biz.overPrice(this.orderDetail["orderId"], Number(pageData?.items?.price))
+            return Biz.dialog("***main() OverPrice")
+        }
     }
 
-    this.pInfo["limitPrice"] = orderDetail.limitPrice
-    this.pInfo["status"] = orderDetail.status
-    this.pInfo["orderId"] = orderDetail.orderId
-    if (Biz.ifSuccess(this.pInfo)) {
-      return console.log("****this order already success:", orderDetail);
-    }
-    let old = await Utils.STORE_GET_ITEM(orderDetail.orderId);
-    old.updateTime = Utils.formatDateStr(productInformation["終了日時"]);
-    await Utils.STORE_SET_ITEM(orderDetail.orderId, Object.assign(old, this.pInfo));
-    const pageData = Utils.getPageDataJSON();
-    if (Number(pageData?.items?.price) > orderDetail.limitPrice) {
-      Biz.overPrice(this.orderDetail["orderId"])
-      return Biz.dialog("***main() OverPrice")
-    }
-  }
-
-  createConnect() {
-    if (Utils.isAuctionUrl(location.pathname)) {
-      $(`<div id="time_left" style="font-size: 28px;color: red;"></div>`).insertBefore($("#acWrGlobalNavi"))
+    createConnect() {
+        if (Utils.isAuctionUrl(location.pathname)) {
+            $(`<div id="time_left" style="font-size: 28px;color: red;"></div>`).insertBefore($("#acWrGlobalNavi"))
 // Connect to the background script
-      Biz.port = chrome.runtime.connect({name: "GHJ-port-" + auctionId});
+            Biz.port = chrome.runtime.connect({name: "GHJ-port-" + auctionId});
 // Listen for messages from the background script
-      Biz.port.onMessage.addListener(function (message) {
-        if (message.action === "do_auction") {
-          timePaint();
-        } else if (message.action === "call_checkObject") {
-          checkObject();
+            Biz.port.onMessage.addListener(function (message) {
+                if (message.action === "do_auction") {
+                    timePaint();
+                } else if (message.action === "call_checkObject") {
+                    checkObject();
+                }
+            });
+
         }
-      });
 
     }
 
-  }
+    offerBid(day: number, hour: number, min: number, sec: number) {
+        //  check if the time is correct
+        const othersBidCount = Number($(".Count .Count__link .Count__detail").text().slice(0, -1))
+        if (othersBidCount > 1) {
+            if (!(day == 0 && hour == 0 && min == 0 && sec <= 5)) {
+                return;
+            }
+        } else {
+            if (!(day == 0 && hour == 0 && min == 0 && sec <= 4)) {
+                return;
+            }
+        }
+        // check order status
+        // @ts-ignore
+        if (!this.orderDetail || this.orderDetail.status != 1) {
+            return;
+        }
+        // check currently prices
 
-  offerBid(day: number, hour: number, min: number, sec: number) {
-    //  check if the time is correct
-    const othersBidCount = Number($(".Count .Count__link .Count__detail").text().slice(0, -1))
-    if (othersBidCount > 1) {
-      if (!(day == 0 && hour == 0 && min == 0 && sec <= 5)) {
-        return;
-      }
-    } else {
-      if (!(day == 0 && hour == 0 && min == 0 && sec <= 4)) {
-        return;
-      }
+        const Price__value = Number($(".Price__value").text().split("円").shift()?.replace(/,/g, ""));
+        // @ts-ignore
+        if (Price__value > this.orderDetail.limitPrice) {
+            Biz.overPrice(this.orderDetail["orderId"], Price__value)
+            return Biz.dialog("***offerBid OverPrice check currently prices")
+        }
+        //1. bid
+        Biz.bid();
+        //2. can not upper the limit price
+        if (!Biz.isGoodPrice(this.orderDetail["limitPrice"])) {
+            Biz.overPrice(this.orderDetail["orderId"], (document.querySelector("[name=\"Bid\"]") as HTMLInputElement)?.value)
+            return Biz.dialog("***offerBid OverPricec an not upper the limit price")
+        }
+        setTimeout(function () {
+            Utils.clickWithSelector(".js-validator-submit");
+        }, 1);
+        //3. 確認する
     }
-    // check order status
-    // @ts-ignore
-    if (!this.orderDetail || this.orderDetail.status != 1) {
-      return;
-    }
-    // check currently prices
-    // @ts-ignore
-    if (Number($(".Price__value").text().split("円").shift()?.replace(/,/g, "")) > this.orderDetail.limitPrice) {
-      Biz.overPrice(this.orderDetail["orderId"])
-      return Biz.dialog("***offerBid OverPrice check currently prices")
-    }
-    //1. bid
-    Biz.bid();
-    //2. can not upper the limit price
-    if (!Biz.isGoodPrice(this.orderDetail["limitPrice"])) {
-
-      Biz.overPrice(this.orderDetail["orderId"])
-      return Biz.dialog("***offerBid OverPricec an not upper the limit price")
-    }
-    setTimeout(function () {
-      Utils.clickWithSelector(".js-validator-submit");
-    }, 1);
-    //3. 確認する
-  }
 }
 
 let auctionId = location.pathname.split("/").pop() as string;
 const reloadTime = Utils.rangeNumber(150, 260);
 const reloadTimeLong = Utils.rangeNumber(160, 180);
-const reloadCut1 = Utils.rangeNumber(8, 20);
+const reloadCut1 = Utils.rangeNumber(9, 20);
 let timeLeft = -10;
 let isFirstPaint = true
 let timeSinceLast = 0;
@@ -119,124 +120,124 @@ myInstance.main();
 timePaint();
 
 function createXMLHttp() {
-  try {
-    // @ts-ignore
-    return new ActiveXObject("Msxml2.XMLHTTP");
-  } catch (e) {
     try {
-      // @ts-ignore
-      return new ActiveXObject("Microsoft.XMLHTTP");
+        // @ts-ignore
+        return new ActiveXObject("Msxml2.XMLHTTP");
     } catch (e) {
-      try {
-        return new XMLHttpRequest();
-      } catch (e) {
-      }
+        try {
+            // @ts-ignore
+            return new ActiveXObject("Microsoft.XMLHTTP");
+        } catch (e) {
+            try {
+                return new XMLHttpRequest();
+            } catch (e) {
+            }
+        }
     }
-  }
-  return null;
+    return null;
 }
 
 function setPageData(xmlhttp: XMLHttpRequest) {
-  try {
-    //readyState complete(4) is OK
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      // @ts-ignore
-      timeLeft = xmlhttp.responseText;
-      try {
-        Biz.postMessage({action: "auction_timeLeft", timeLeft: +timeLeft});
-      } catch (e) {
-      }
-    }
-  } catch (e) {
+    try {
+        //readyState complete(4) is OK
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            // @ts-ignore
+            timeLeft = xmlhttp.responseText;
+            try {
+                Biz.postMessage({action: "auction_timeLeft", timeLeft: +timeLeft});
+            } catch (e) {
+            }
+        }
+    } catch (e) {
 
-  }
+    }
 }
 
 function checkObject() {
-  if (xmlhttp) {
-    try {
-      xmlhttp.abort();
-      var nowTime = new Date().getTime();
-      xmlhttp.onreadystatechange = function () {
-        setPageData(xmlhttp)
-      };
-      xmlhttp.open("GET", "https://page.auctions.yahoo.co.jp/now?aID=" + auctionId + "&nowtime=" + nowTime, true);
-      xmlhttp.send(null);
+    if (xmlhttp) {
+        try {
+            xmlhttp.abort();
+            var nowTime = new Date().getTime();
+            xmlhttp.onreadystatechange = function () {
+                setPageData(xmlhttp)
+            };
+            xmlhttp.open("GET", "https://page.auctions.yahoo.co.jp/now?aID=" + auctionId + "&nowtime=" + nowTime, true);
+            xmlhttp.send(null);
 
-    } catch (e) {
-      if (!isFirstPaint) {
-        window.location.reload();
-      }
-    }
+        } catch (e) {
+            if (!isFirstPaint) {
+                window.location.reload();
+            }
+        }
 
-  } else {
-    if (!isFirstPaint) {
-      window.location.reload();
+    } else {
+        if (!isFirstPaint) {
+            window.location.reload();
+        }
     }
-  }
 }
 
 
 let timeLeftEle = document.getElementById("time_left");
 
 function timePaint() {
-  if (!Utils.isAuctionUrl(location.pathname)) {
-    return;
-  }
-
-  if (outputString == Biz.BID_OVER_NAME) {
-    return Biz.disconnect(auctionId, outputString);
-  }
-  if (timeLeft == reloadCut1) {
-    return window.location.reload();
-  }
-  if (timeLeft > 300 && timeSinceLast >= reloadTimeLong) {
-    return window.location.reload();
-  }
-
-  if (isFirstPaint || timeLeft == -1 || (timeLeft < 300 && timeSinceLast >= 60)) {
-
-    if ((timeLeft < 75 && timeSinceLast >= reloadTime)) {
-      return window.location.reload();
+    if (!Utils.isAuctionUrl(location.pathname)) {
+        return;
     }
-    checkObject();
-    isFirstPaint = false;
-    timeSinceLast = 0;
-  }
 
-  if (timeLeft === -10) {
-    return;
-  }
+    if (outputString == Biz.BID_OVER_NAME) {
+        return Biz.disconnect(auctionId, outputString);
+    }
+    if (timeLeft == reloadCut1) {
+        return window.location.reload();
+    }
+    if (timeLeft > 300 && timeSinceLast >= reloadTimeLong) {
+        return window.location.reload();
+    }
 
-  if (timeLeft <= 0) {
-    outputString = Biz.BID_OVER_NAME;
-    Biz.disconnect(auctionId, outputString);
-    Biz.ifSuccess(myInstance.pInfo);
-  } else {
-    var day = Math.floor(timeLeft / 86400);
-    var hour = Math.floor((timeLeft - day * 86400) / 3600);
-    var min = Math.floor((timeLeft - (day * 86400) - (hour * 3600)) / 60);
-    var sec = timeLeft - (day * 86400) - (hour * 3600) - (min * 60);
+    if (isFirstPaint || timeLeft == -1 || (timeLeft < 300 && timeSinceLast >= 60)) {
 
+        if ((timeLeft < 75 && timeSinceLast >= reloadTime)) {
+            return window.location.reload();
+        }
+        checkObject();
+        isFirstPaint = false;
+        timeSinceLast = 0;
+    }
 
-    if (day > 0) {
-      outputString = day + "日＋" + ((hour > 0) ? hour + ":" : "") + ((min < 10) ? "0" + min : min) + ":" + ((sec < 10) ? "0" + sec : sec);
+    if (timeLeft === -10) {
+        return;
+    }
+
+    if (timeLeft <= 0) {
+        outputString = Biz.BID_OVER_NAME;
+        Biz.disconnect(auctionId, outputString);
+        Biz.ifSuccess(myInstance.pInfo);
     } else {
-      outputString = ((hour > 0) ? hour + ":" : "") + ((min < 10) ? "0" + min : min) + ":" + ((sec < 10) ? "0" + sec : sec);
-    }
-    timeLeft -= 1;
-    timeSinceLast += 1;
+        var day = Math.floor(timeLeft / 86400);
+        var hour = Math.floor((timeLeft - day * 86400) / 3600);
+        var min = Math.floor((timeLeft - (day * 86400) - (hour * 3600)) / 60);
+        var sec = timeLeft - (day * 86400) - (hour * 3600) - (min * 60);
 
-    if (outputString != Biz.BID_OVER_NAME) {
-      myInstance.offerBid(day, hour, min, sec);
+
+        if (day > 0) {
+            outputString = day + "日＋" + ((hour > 0) ? hour + ":" : "") + ((min < 10) ? "0" + min : min) + ":" + ((sec < 10) ? "0" + sec : sec);
+        } else {
+            outputString = ((hour > 0) ? hour + ":" : "") + ((min < 10) ? "0" + min : min) + ":" + ((sec < 10) ? "0" + sec : sec);
+        }
+        timeLeft -= 1;
+        timeSinceLast += 1;
+
+        if (outputString != Biz.BID_OVER_NAME) {
+            myInstance.offerBid(day, hour, min, sec);
+        }
     }
-  }
-  if (!timeLeftEle) {
-    timeLeftEle = document.getElementById("time_left");
-  }
-  if (timeLeftEle) {
-    timeLeftEle.innerText = "残り時間:" + outputString;
-  }
-  // console.log("****残り時間:", outputString);
+    if (!timeLeftEle) {
+        timeLeftEle = document.getElementById("time_left");
+    }
+    if (timeLeftEle) {
+        timeLeftEle.innerText = "残り時間:" + outputString;
+    }
+    // console.log("****残り時間:", outputString);
 }
 
--- src/utils/biz.ts
+++ src/utils/biz.ts
@@ -3,208 +3,208 @@ import {Utils} from "src/utils/utils";
 
 export class Biz {
 
-  /**
-   * 1.商品情報
-   */
-  static getProductInformation() {
-    const productInfor = $(".ProductInformation__item .Section__table tr")
-    if (productInfor.length) {
-      const details: { [x: string]: string } = {}
-      productInfor.each((function () {
-        details[$(this).find("th").text().trim()] = $(this).find("td").text()?.trim().replace(/[\s\\\n]+/, "").trim();
-      }))
-      delete details["カテゴリ"]
-      return details;
-    }
-    return false;
-  }
-
-  /**
-   * 2.入札
-   */
-  static bid() {
-    Utils.clickWithSelector(".Button--bid")
-  }
-
-  /**
-   * 上記に同意して
-   * 入札する
-   */
-  static agreeBid() {
-    if (location.pathname == "/jp/show/bid_preview") {
-      Utils.clickWithSelector(".SubmitBox__button--bid")
-      if (!document.querySelector(".SubmitBox__button--bid")) {
-        location.href = $("#3DmodFootLink a").prop("href");
-      }
-    }
-  }
-
-  static isGoodPrice(highestPrice: number): boolean {
-    let bidInput = document.querySelector("[name=\"Bid\"]") as HTMLInputElement;
-    if (!bidInput) {
-      return false;
-    }
-
-    return Number(bidInput.value) < highestPrice;
-  }
-
-
-  static async resultPage() {
-    if (location.pathname == "/jp/config/placebid") {
-      if (location.search) {
-        location.href = $(".CompleteMain__ohterLinkItem").eq(0).find("a").prop("href");
-      } else {
-        if ($(".CompleteMain__title").text() == "入札を受け付けました。あなたが現在の最高額入札者です。") {
-          location.href = $(".CompleteMain__ohterLinkItem").eq(0).find("a").prop("href");
-        } else if ($("#3DmodFootLink a").text() == "商品ページに戻る") {
-          location.href = $("#3DmodFootLink a").prop("href");
-        } else {
-          this.reBid();
+    /**
+     * 1.商品情報
+     */
+    static getProductInformation() {
+        const productInfor = $(".ProductInformation__item .Section__table tr")
+        if (productInfor.length) {
+            const details: { [x: string]: string } = {}
+            productInfor.each((function () {
+                details[$(this).find("th").text().trim()] = $(this).find("td").text()?.trim().replace(/[\s\\\n]+/, "").trim();
+            }))
+            delete details["カテゴリ"]
+            return details;
         }
-      }
-    } else if (location.pathname == "/jp/config/undefined") {
-      if (!location.search) {
-        let prop = $(".l-contentsFoot .u-TextCenter a").prop("href");
-        if (prop) {
-          return location.href = prop;
+        return false;
+    }
+
+    /**
+     * 2.入札
+     */
+    static bid() {
+        Utils.clickWithSelector(".Button--bid")
+    }
+
+    /**
+     * 上記に同意して
+     * 入札する
+     */
+    static agreeBid() {
+        if (location.pathname == "/jp/show/bid_preview") {
+            Utils.clickWithSelector(".SubmitBox__button--bid")
+            if (!document.querySelector(".SubmitBox__button--bid")) {
+                location.href = $("#3DmodFootLink a").prop("href");
+            }
+        }
+    }
+
+    static isGoodPrice(highestPrice: number): boolean {
+        let bidInput = document.querySelector("[name=\"Bid\"]") as HTMLInputElement;
+        if (!bidInput) {
+            return false;
         }
 
-        history.back();
-      }
+        return Number(bidInput.value) <= highestPrice;
     }
-  }
 
-  static async reBid() {
-    var orderId = $("[name=\"ItemID\"]").val() as string;
-    let orderDetail = await Utils.STORE_GET_ITEM(orderId);
-    if (orderDetail.remark) {
-      location.href = $(".l-contentsFoot .u-TextCenter a").prop("href")
-      return console.log("*****rebid already:", orderDetail)
-    }
-    //2. can not upper the limit price
-    if (!Biz.isGoodPrice(orderDetail["limitPrice"])) {
-      this.overPrice(orderDetail.orderId)
-      return this.dialog("*****reBid()OverPrice");
-    }
-    orderDetail.remark = true
-    Utils.STORE_SET_ITEM(orderId, orderDetail)
-    setTimeout(function () {
-      Utils.clickWithSelector(".SubmitBox__button--rebid");
-    }, 1);
-  }
 
-  static searchPage() {
-    let element = document.querySelector("#sbn [name=\"p\"]") as HTMLInputElement;
-    if (element && location.pathname == "/search/search") {
-      $(`
+    static async resultPage() {
+        if (location.pathname == "/jp/config/placebid") {
+            if (location.search) {
+                location.href = $(".CompleteMain__ohterLinkItem").eq(0).find("a").prop("href");
+            } else {
+                if ($(".CompleteMain__title").text() == "入札を受け付けました。あなたが現在の最高額入札者です。") {
+                    location.href = $(".CompleteMain__ohterLinkItem").eq(0).find("a").prop("href");
+                } else if ($("#3DmodFootLink a").text() == "商品ページに戻る") {
+                    location.href = $("#3DmodFootLink a").prop("href");
+                } else {
+                    this.reBid();
+                }
+            }
+        } else if (location.pathname == "/jp/config/undefined") {
+            if (!location.search) {
+                let prop = $(".l-contentsFoot .u-TextCenter a").prop("href");
+                if (prop) {
+                    return location.href = prop;
+                }
+
+                history.back();
+            }
+        }
+    }
+
+    static async reBid() {
+        var orderId = $("[name=\"ItemID\"]").val() as string;
+        let orderDetail = await Utils.STORE_GET_ITEM(orderId);
+        if (orderDetail.remark) {
+            location.href = $(".l-contentsFoot .u-TextCenter a").prop("href")
+            return console.log("*****rebid already:", orderDetail)
+        }
+        //2. can not upper the limit price
+        if (!Biz.isGoodPrice(orderDetail["limitPrice"])) {
+            this.overPrice(orderDetail.orderId, (document.querySelector("[name=\"Bid\"]") as HTMLInputElement)?.value)
+            return this.dialog("*****reBid()OverPrice");
+        }
+        orderDetail.remark = true
+        Utils.STORE_SET_ITEM(orderId, orderDetail)
+        setTimeout(function () {
+            Utils.clickWithSelector(".SubmitBox__button--rebid");
+        }, 1);
+    }
+
+    static searchPage() {
+        let element = document.querySelector("#sbn [name=\"p\"]") as HTMLInputElement;
+        if (element && location.pathname == "/search/search") {
+            $(`
         <button id="save-keywords" style="font-size: 18px;color: white; background: green; padding: 10px 15px; border-radius: 5px;">My保存した検索条件</button>
       `).insertBefore("#sbn");
-      $("#save-keywords").on("click", () => {
-        Utils.STORE_SET_ITEM("keywords" + element.value, {keywords: element.value, url: location.href});
-        $("#save-keywords").remove();
-      })
-    }
-  }
-
-
-  static otherPage() {
-    this.agreeBid();
-    this.searchPage();
-    this.resultPage();
-  }
-
-  static port: chrome.runtime.Port
-
-  static postMessage(msg: any) {
-    try {
-      if (this.port) {
-        this.port.postMessage(msg);
-      } else {
-        if (msg.action == "auction_closeTab") {
-          // chrome.runtime.connect({name: "GHJ-port-auction_closeTab"});
+            $("#save-keywords").on("click", () => {
+                Utils.STORE_SET_ITEM("keywords" + element.value, {keywords: element.value, url: location.href});
+                $("#save-keywords").remove();
+            })
         }
-      }
-    } catch (e) {
     }
-  }
 
-  static dialog(msg: string) {
-    return console.log(msg);
-  }
 
-  static disconnect(id?: any, msg?: string) {
-    // Utils.STORE_DELETE_ITEM(id);
-    this.postMessage({action: "auction_closeTab", msg: msg})
-    this.port?.disconnect();
-    if (id && msg == this.BID_OVER_NAME) {
-      this.updateBidItem({orderId: id, status: 5, remark: msg + ""})
+    static otherPage() {
+        this.agreeBid();
+        this.searchPage();
+        this.resultPage();
     }
-  }
 
-  static overPrice(id: string) {
-    this.disconnect(id, "Over Price")
-    return this.updateBidItem({orderId: id, status: 3, remark: "Over Price"})
-  }
+    static port: chrome.runtime.Port
 
-  static async updateBidItem(data: IBidItem) {
-    const old = await Utils.STORE_GET_ITEM(data.orderId);
-    await Utils.STORE_SET_ITEM(data.orderId, Object.assign(old, data));
-  }
+    static postMessage(msg: any) {
+        try {
+            if (this.port) {
+                this.port.postMessage(msg);
+            } else {
+                if (msg.action == "auction_closeTab") {
+                    // chrome.runtime.connect({name: "GHJ-port-auction_closeTab"});
+                }
+            }
+        } catch (e) {
+        }
+    }
+
+    static dialog(msg: string) {
+        return console.log(msg);
+    }
+
+    static disconnect(id?: any, msg?: string) {
+        // Utils.STORE_DELETE_ITEM(id);
+        this.postMessage({action: "auction_closeTab", msg: msg})
+        this.port?.disconnect();
+        if (id && msg == this.BID_OVER_NAME) {
+            this.updateBidItem({orderId: id, status: 5, remark: msg + ""})
+        }
+    }
+
+    static overPrice(id: string, price: any) {
+        this.disconnect(id, "Over Price " + price)
+        return this.updateBidItem({orderId: id, status: 3, remark: "Over Price"})
+    }
+
+    static async updateBidItem(data: IBidItem) {
+        const old = await Utils.STORE_GET_ITEM(data.orderId);
+        await Utils.STORE_SET_ITEM(data.orderId, Object.assign(old, data));
+    }
 
 
-  static showAddJobButton(productInformation: any) {
-    $(`
+    static showAddJobButton(productInformation: any) {
+        $(`
         <div id="save-bidJob-parent">
             <input type="number" style="border: red solid 5px;font-size:35px;height: 60px; width: 89%; " id="save-bidJob-input" placeholder="私の最高額入">
             <button class="save-bidJob"  style="font-size: 28px; height: 80px; width: 89%; border-radius: 10px; color: white;background: red; ">今すぐ入札</button>
         </div>
       `).insertBefore("#ProductTitle");
-    const handler = () => {
-      const url = location.href;
-      let price = Number($("#save-bidJob-input").val());
-      if (price > 0) {
-        Utils.STORE_SET_ITEM(url.split("/").pop() as string, {
-          orderId: url.split("/").pop(),
-          limitPrice: price,
-          remark: $(".yjmthloginarea strong").text(),
-          info: $(".ProductImage__body .ProductImage__image.is-on img").prop("src"),
-          updateTime: Utils.formatDateStr(productInformation["終了日時"]),
-          url: url,
-          status: 1,
-        }).then(() => {
-          location.reload();
-        })
-      } else {
-        alert(`私の最高額入:${price} 再入力`)
-      }
-    };
-    $('#save-bidJob-input').on('change',handler)
+        const handler = () => {
+            const url = location.href;
+            let price = Number($("#save-bidJob-input").val());
+            if (price > 0) {
+                Utils.STORE_SET_ITEM(url.split("/").pop() as string, {
+                    orderId: url.split("/").pop(),
+                    limitPrice: price,
+                    remark: $(".yjmthloginarea strong").text(),
+                    info: $(".ProductImage__body .ProductImage__image.is-on img").prop("src"),
+                    updateTime: Utils.formatDateStr(productInformation["終了日時"]),
+                    url: url,
+                    status: 1,
+                }).then(() => {
+                    location.reload();
+                })
+            } else {
+                alert(`私の最高額入:${price} 再入力`)
+            }
+        };
+        $("#save-bidJob-input").on("change", handler)
 
-    $(".save-bidJob").on("click", handler)
-  }
+        $(".save-bidJob").on("click", handler)
+    }
 
-  static ifSuccess(pInfo: any) {
-    let b = $(".Button--proceed").text() == "取引ナビ"
-    if (pInfo && (pInfo.status == 1 || pInfo.status == 5)) {
-      if (pInfo["落札者"] && pInfo["落札者"] != "なし") {
-        if (!(pInfo["落札者"] + "").includes("***")) {
-          b = true;
+    static ifSuccess(pInfo: any) {
+        let b = $(".Button--proceed").text() == "取引ナビ"
+        if (pInfo && (pInfo.status == 1 || pInfo.status == 5)) {
+            if (pInfo["落札者"] && pInfo["落札者"] != "なし") {
+                if (!(pInfo["落札者"] + "").includes("***")) {
+                    b = true;
+                }
+            }
         }
-      }
+
+        if (b) {
+            this.disconnect(pInfo.orderId, "have success");
+            this.updateBidItem({
+                orderId: pInfo.orderId,
+                status: 2,
+                remark: $(".yjmthloginarea strong").text() + "," + $(".Price .Price__value").contents().filter(function () {
+                    return this.nodeType === Node.TEXT_NODE;
+                }).text().trim()
+            })
+        }
+        return b;
     }
 
-    if (b) {
-      this.disconnect(pInfo.orderId, "have success");
-      this.updateBidItem({
-        orderId: pInfo.orderId,
-        status: 2,
-        remark: $(".yjmthloginarea strong").text() + "," + $(".Price .Price__value").contents().filter(function () {
-          return this.nodeType === Node.TEXT_NODE;
-        }).text().trim()
-      })
-    }
-    return b;
-  }
-
-  static BID_OVER_NAME = "オークション - 終了"
+    static BID_OVER_NAME = "オークション - 終了"
 }
