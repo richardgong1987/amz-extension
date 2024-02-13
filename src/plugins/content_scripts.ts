9d38abb55678342a9d84dbb6a342ab3a691e51af
pc <pc@pc.com>
2024-02-13 19:00:28 +0900

    updated

--- src/plugins/content_scripts.ts
+++ src/plugins/content_scripts.ts
@@ -6,18 +6,49 @@ port.onMessage.addListener((msg) => {
 });
 
 setTimeout(() => {
-  var child: { twinCatName: string | undefined; twinCatInfo: string; }[] = [];
-  $(".wrap ul li a").each(function () {
-    child.push({
-      "twinCatName": $(this).attr("href")?.split("/").filter(v => v).pop(),
-      "twinCatInfo": $(this).find(".name").text(),
-    })
-  });
+  var child: any = []
+  if ($(".wrap").length) {
+    if ($(".wrap").length > 2) {
+      $(".wrap").each(function () {
+        var $wrap = $(this);
+        $wrap.find("ul li a").each(function () {
+          child.push({
+            "twinCatName": $(this).attr("href")?.split("/").filter(v => v).pop(),
+            "twinCatInfo": $wrap.find("h3").text() + $(this).find("h3").text(),
+          });
+        });
+      });
+    } else {
+      $(".wrap ul li a").each(function () {
+        child.push({
+          "twinCatName": $(this).attr("href")?.split("/").filter(v => v).pop(),
+          "twinCatInfo": $(this).text(),
+        })
+      });
+    }
+
+  } else if ($(".send_sp").length) {
+    if ($(".send_sp li").length) {
+      $(".send_sp li a").each(function () {
+        child.push({
+          "twinCatName": $(this).attr("href")?.split("/").filter(v => v).pop(),
+          "twinCatInfo": $(this).text()
+        })
+      });
+    } else {
+      $(".send_sp").next().find("li a").each(function () {
+        child.push({
+          "twinCatName": $(this).attr("href")?.split("/").filter(v => v).pop(),
+          "twinCatInfo": $(this).text()
+        })
+      });
+    }
+  }
   const msg = {
     action: "appendData",
     data: child
   }
-  // port.postMessage(msg);
-}, 5000);
+  port.postMessage(msg);
+}, 3000);
 
 
